76ea7e9bbe75454fc8d11327dda8e8acddef06b4
Vitaly Turovsky <vital2580@icloud.com>
2022-03-14 23:38:10 +0300

    feat: volar and other ext support

--- src/util.ts
+++ src/util.ts
@@ -3,9 +3,25 @@ import * as vscode from 'vscode'
 export const interpolationPropRegex = /(?::|@|v-)(\w|-)+="([^"]*)$/
 
 // TODO! caching
+let veturMode: boolean | undefined
+export const getNormalizedOutline = async (documentUri: vscode.Uri) => {
+    if (veturMode === undefined) {
+        const updateMode = () => {
+            // support volar
+            veturMode = !!vscode.extensions.getExtension('octref.vetur')
+        }
+
+        vscode.extensions.onDidChange(updateMode)
+        updateMode()
+    }
+
+    const outline: vscode.DocumentSymbol[] = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', documentUri)
+    return veturMode ? outline[0]?.children : outline
+}
+
 export const getDefaultExportOutline = async (documentUri: vscode.Uri) => {
-    const result: vscode.DocumentSymbol[] = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', documentUri)
-    const defaultExport = result[0]?.children.find(({ name }) => name === 'script')?.children.find(({ name }) => name === 'default')
+    const outline = await getNormalizedOutline(documentUri)
+    const defaultExport = outline?.find(({ name }) => name === 'script')?.children.find(({ name }) => name === 'default')
     if (!defaultExport) console.warn("Can' get default outline from Vetur")
 
     return defaultExport
