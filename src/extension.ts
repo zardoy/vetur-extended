138da519d88e4789aed998354a55817c46e5a0cd
Vitaly Turovsky <vital2580@icloud.com>
2022-05-18 12:25:07 +0300

    implement picking component name

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,5 @@
+import * as vscode from 'vscode'
+import { registerActiveDevelopmentCommand } from 'vscode-framework'
 import { registerGenerateJsconfigAliases } from './commands/generateJsconfigAliases'
 import { registerFindReferences } from './findReferences'
 import { registerGotoDefinition } from './gotoDefinition'
@@ -12,4 +14,26 @@ export const activate = () => {
     registerHover()
     registerGenerateJsconfigAliases()
     // registerLinksProvider()
+    registerActiveDevelopmentCommand(async () => {
+        const editor = vscode.window.activeTextEditor
+        if (!editor) return
+        const { document } = editor
+        const componentRange = document.getWordRangeAtPosition(editor.selection.end, /<\/?([-\d\w])+/) // (?=(\s|\/?>))
+        if (!componentRange) return
+        // const prefixText = document.getText(wordRange.with(wordRange.start.translate(0, -2), wordRange.start))
+        const rangeText = document.getText(componentRange)
+        const componentName = /<\/?(([-\d\w])+)/.exec(rangeText)?.[1]
+        console.log('componentName', componentName)
+        // console.time('highlights')
+        // const highlights: vscode.DocumentHighlight[] = await vscode.commands.executeCommand(
+        //     'vscode.executeDocumentHighlights',
+        //     editor.document.uri,
+        //     editor.selection.end,
+        // )
+        // console.timeEnd('highlights')
+        // console.log(
+        //     'highlights',
+        //     highlights.map(highlight => [vscode.DocumentHighlightKind[highlight.kind! ?? -1], highlight.range.start, highlight.range.end]),
+        // )
+    })
 }
