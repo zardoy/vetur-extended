0678ff7024295a6a245a37e1f55e93582c428750
Ilya Golovin <74474615+ILScc@users.noreply.github.com>
2022-05-18 19:13:48 +0300

    feat: add definitions (goto) for copmponents in `<template>` (#4)
    Co-authored-by: @ILScc
    - It brings restriction for using generics in TS script, since it is enabled globally per vue file
    - Can't be via disabling `enableLinks`

--- src/componentsLinks.ts
+++ src/componentsLinks.ts
@@ -4,23 +4,30 @@ import { getExtensionSetting } from 'vscode-framework'
 import { firstExists } from '@zardoy/vscode-utils/build/fs'
 import { Utils } from 'vscode-uri'
 
+export const importsCache = new Map<string, vscode.Uri>()
+
 export const registerComponentsLinks = () => {
+    vscode.workspace.onDidChangeTextDocument(() => {
+        importsCache.clear()
+    })
+
     vscode.languages.registerDocumentLinkProvider('vue', {
-        async provideDocumentLinks(document, token) {
+        async provideDocumentLinks(document) {
+            const activeEditor = vscode.window.activeTextEditor!
             if (!getExtensionSetting('enableLinks')) return
 
             const text = document.getText()
-            const matches: RegExpMatchArray[] = [...text.matchAll(/(\s*import .+ from )(['"].+['"])/g)]
+            const matches: RegExpMatchArray[] = [...text.matchAll(/(import (.+) from )(['"].+['"])/g)]
             const links: vscode.DocumentLink[] = []
             for (const match of matches) {
-                if (!['./', '../'].some(predicate => match[2]!.slice(1).startsWith(predicate))) continue
+                const importingPath = match[3]!;
+                if (!['./', '../'].some(predicate => importingPath.slice(1).startsWith(predicate))) continue
 
                 const startIdx = match.index! + match[1]!.length
-                const endIdx = startIdx + match[2]!.length
-                let path = match[2]!.slice(1, -1)
+                const endIdx = startIdx + importingPath.length
+                let path = importingPath.slice(1, -1)
                 const questionmarkIndex = /\?/.exec(path)?.index
                 if (questionmarkIndex) path = path.slice(0, questionmarkIndex)
-
                 const importUri = vscode.Uri.joinPath(Utils.dirname(document.uri), path)
                 const targetUri = await firstExists(
                     ['/index.vue', '', '.vue'].map(ext => {
@@ -31,6 +38,9 @@ export const registerComponentsLinks = () => {
                     }),
                 )
                 if (targetUri === undefined) continue
+                const importingIdentifier = match[2]!
+                // links can be updated in random order if several editors are opened
+                if (/(\w|\d)+/.test(importingIdentifier) && document.uri === activeEditor.document.uri) importsCache.set(importingIdentifier, targetUri)
 
                 links.push({
                     range: new vscode.Range(document.positionAt(startIdx), document.positionAt(endIdx)),
