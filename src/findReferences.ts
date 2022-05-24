1aa4b2a794fb9e1b7331619f51c0ad6de843a039
Vitaly Turovsky <vital2580@icloud.com>
2022-05-25 00:58:49 +0300

    feat: improve `findComponentReferences` now seeks for kebab-case variant as well

--- src/findReferences.ts
+++ src/findReferences.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode'
 import escapeStringRegexp from 'escape-string-regexp'
 import { getExtensionSetting, registerExtensionCommand } from 'vscode-framework'
+import { paramCase as kebabCase } from 'change-case'
+import { getDefaultExportOutline } from './util'
 
 export const registerFindReferences = () => {
     vscode.languages.registerReferenceProvider('vue', {
-        async provideReferences(document, position, { includeDeclaration }) {
+        async provideReferences(document, position) {
             // ignoring includeDeclaration for now
             if (!/^\s*export default {$/.test(document.lineAt(position.translate(-1)).text)) return
             const match = /^(\s*name: ?)(['"])(.+)\2,?$/.exec(document.getText(document.lineAt(position).range))
@@ -26,22 +28,23 @@ export const registerFindReferences = () => {
                         let inTemplate = false
                         for (let [lineIndex, line] of contents.split('\n').entries()) {
                             // win \n\r
-                            if (line.endsWith('\r')) line = line.slice(0, -1)
+                            line = line.trim()
                             if (line === '<template>') {
                                 inTemplate = true
                                 continue
                             }
 
                             if (line === '</template>') break
-                            if (!inTemplate) continue;
-                                // TODO! use iterator
-                                const matches = line.matchAll(new RegExp(`<${escapeStringRegexp(componentName)}`, 'g'))
-                                for (const match of matches) {
-                                    if (match.index === undefined) continue
-                                    const startPos = new vscode.Position(lineIndex, match.index + 1)
-                                    ranges.push(new vscode.Range(startPos, startPos.translate(0, componentName.length)))
-                                }
-
+                            if (!inTemplate) continue
+                            // TODO! use iterator
+                            const matches = line.matchAll(
+                                new RegExp(`<(${escapeStringRegexp(componentName)}|${escapeStringRegexp(kebabCase(componentName))})`, 'g'),
+                            )
+                            for (const match of matches) {
+                                if (match.index === undefined) continue
+                                const startPos = new vscode.Position(lineIndex, match.index + 1)
+                                ranges.push(new vscode.Range(startPos, startPos.translate(0, componentName.length)))
+                            }
                         }
 
                         return {
@@ -60,11 +63,8 @@ export const registerFindReferences = () => {
     registerExtensionCommand('findComponentReferences', async () => {
         const activeEditor = vscode.window.activeTextEditor
         if (activeEditor === undefined || activeEditor.viewColumn === undefined) return
-        const result: vscode.DocumentSymbol[] = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', activeEditor.document.uri)
-        const range = result[0]?.children
-            .find(({ name }) => name === 'script')
-            ?.children.find(({ name }) => name === 'default')
-            ?.children.find(({ name }) => name === 'name')?.range
+        const defaultExport = await getDefaultExportOutline(activeEditor.document.uri)
+        const range = defaultExport?.children.find(({ name }) => name === 'name')?.range
         if (!range) return
         activeEditor.selections = [new vscode.Selection(range.end, range.end)]
         await vscode.commands.executeCommand('editor.action.referenceSearch.trigger')
