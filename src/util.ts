3fdd7a928078ae3913f07bdb6f74f3701e65a3e1
Vitaly Turovsky <vital2580@icloud.com>
2022-05-25 22:01:36 +0300

    refactor: use shared getNormalizedVueOutline from utils

--- src/util.ts
+++ src/util.ts
@@ -1,26 +1,10 @@
 import * as vscode from 'vscode'
+import { getNormalizedVueOutline } from '@zardoy/vscode-utils/build/vue'
 
 export const interpolationPropRegex = /(?::|@|v-)([-\d\w])+="([^"]*)$/
 
-// TODO! caching
-let veturMode: boolean | undefined
-export const getNormalizedOutline = async (documentUri: vscode.Uri) => {
-    if (veturMode === undefined) {
-        const updateMode = () => {
-            // support volar
-            veturMode = !!vscode.extensions.getExtension('octref.vetur')
-        }
-
-        vscode.extensions.onDidChange(updateMode)
-        updateMode()
-    }
-
-    const outline: vscode.DocumentSymbol[] = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', documentUri)
-    return veturMode ? outline[0]?.children : outline
-}
-
 export const getDefaultExportOutline = async (documentUri: vscode.Uri) => {
-    const outline = await getNormalizedOutline(documentUri)
+    const outline = await getNormalizedVueOutline(documentUri)
     const defaultExport = outline?.find(({ name }) => name === 'script')?.children.find(({ name }) => name === 'default')
     if (!defaultExport) console.warn("Can' get default outline from Vetur")
 
