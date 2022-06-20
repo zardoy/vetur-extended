ac13cce64a7e7dffb3842f2a9f4f52cf6b162ff0
Vitaly Turovsky <vital2580@icloud.com>
2022-06-20 05:47:19 +0300

    fix(completions): exit early on dot-property access (it was just too annoying) fix(completions): skip unknown values

--- src/templateCompletion.ts
+++ src/templateCompletion.ts
@@ -11,12 +11,17 @@ export const registerTemplateCompletion = () => {
             const match = interpolationPropRegex.exec(lineText.slice(0, position.character))
             if (!match) return
             const existingContent = match[2]!
-            console.time('getInfo')
+            // don't make too complicated!
+            const wordRange = document.getWordRangeAtPosition(position) ?? new vscode.Range(position, position)
+            const symbolBefore = document.getText(wordRange.with(wordRange.start.translate(0, -1), wordRange.start))
+            if (['.'].includes(symbolBefore)) return
+            console.time('getOutline')
             const defaultExport = await getDefaultExportOutline(document.uri)
-            console.timeEnd('getInfo')
+            console.timeEnd('getOutline')
             if (!defaultExport) return
-            console.log('compl', defaultExport)
-            const getAllPropsFromDefaultExport = (field: string) => defaultExport.children.find(({ name }) => name === field)?.children.map(({ name }) => name)
+            console.debug('defaultExport', defaultExport)
+            const getAllPropsFromDefaultExport = (field: string) =>
+                defaultExport.children.find(({ name }) => name === field)?.children.map(({ name }) => name)
             const completions = [
                 {
                     kind: vscode.CompletionItemKind.Variable,
@@ -39,7 +44,6 @@ export const registerTemplateCompletion = () => {
                     // }),
                 },
             ]
-            console.log('completions', completions)
             // TODO align with webstorm
             const typeInclude = {
                 'v-model': ['data'],
@@ -47,17 +51,19 @@ export const registerTemplateCompletion = () => {
                 '@': ['methods'],
             }
             return completions
-                .flatMap(({ kind, names }, kindIndex) =>
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-                    names?.map((name, i) => {
-                        kind = +kind as never
-                        if (showCompletions === 'onlyStart' && !name.startsWith(existingContent)) return undefined!
-                        const completion = new vscode.CompletionItem(name, kind)
-                        // TODO recheck src
-                        // force sorting as in source, this workaround is needed as vscode just uses .sort()
-                        completion.sortText = `!${kindIndex + 1}${i.toString().padStart(3, '0')}`
-                        return completion
-                    })!,
+                .flatMap(
+                    ({ kind, names }, kindIndex) =>
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+                        names?.map((name, i) => {
+                            kind = +kind as never
+                            if (name === '<unknown>') return undefined!
+                            if (showCompletions === 'onlyStart' && !name.startsWith(existingContent)) return undefined!
+                            const completion = new vscode.CompletionItem(name, kind)
+                            // TODO recheck src
+                            // force sorting as in source, this workaround is needed as vscode just uses .sort()
+                            completion.sortText = `!${kindIndex + 1}${i.toString().padStart(3, '0')}`
+                            return completion
+                        })!,
                 )
                 .filter(Boolean)
         },
