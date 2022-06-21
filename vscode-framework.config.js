bc5e68013c5a8de54ffc926c2939fdcc7a1dd73e
Vitaly Turovsky <vital2580@icloud.com>
2022-06-21 10:55:09 +0300

    ci: fix postinstall silent crash

--- vscode-framework.config.js
+++ vscode-framework.config.js
@@ -1,13 +1,9 @@
 //@ts-check
 const { defineConfig } = require('@zardoy/vscode-utils/build/defineConfig.cjs')
-const nodeModulesPolyfill = require('@esbuild-plugins/node-modules-polyfill')
 
 module.exports = defineConfig({
     development: {
         disableExtensions: false,
-    },
-    esbuild: {
-        // plugins: [nodeModulesPolyfill.default()],
     },
     target: { desktop: true, web: true },
 })
