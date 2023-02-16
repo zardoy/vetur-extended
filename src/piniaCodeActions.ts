46ceb5fa17863395795f21ee58cfc801a076a1a4
Ilya Golovin <me@ilscc.ru>
2023-02-17 01:53:34 +0300

    feat: make it work

--- src/piniaCodeActions.ts
+++ src/piniaCodeActions.ts
@@ -7,21 +7,32 @@ export const registerPiniaCodeactions = () => {
             const pos = range.start
             const codeActions = [] as vscode.CodeAction[]
 
-            const piniaStoreMatch = new RegExp(/(import (.+)\s+from )(['"].*pinia.*['"])/).exec(document.lineAt(pos.line).text)
+            const piniaStoreMatch = new RegExp(/(import (.+)\s+from )(['"].*pinia.*['"].?)/).exec(document.lineAt(pos.line).text)
 
             if (piniaStoreMatch) {
                 const computedOutline = await getComputedOutline(document.uri)
                 if (!computedOutline) return
 
                 const { range: computedRange } = computedOutline
-
-                const computedStoreTemplate = `\t${piniaStoreMatch[2]!}() {\n\t\t\treturn defineStore('${piniaStoreMatch[2]!}', ${piniaStoreMatch[2]!})()\n\t\t},\n\t`
+                const snippetStirng = // eslint-disable-next-line no-template-curly-in-string
+                    "\t${TM_CURRENT_LINE/import (.+)\\s+from ['\"].*?pinia.*?['\"].?/$1/}() {\n\t\treturn defineStore('$2', $3)()\n\t},$0\n"
 
                 const workspaceEdit = new vscode.WorkspaceEdit()
                 if (computedRange.isSingleLine)
                     // computed: {}; case
-                    workspaceEdit.insert(document.uri, computedRange.end.translate(undefined, -1), `\n\t${computedStoreTemplate}`)
-                else workspaceEdit.insert(document.uri, computedRange.start.translate(1), computedStoreTemplate)
+                    workspaceEdit.set(document.uri, [
+                        new vscode.SnippetTextEdit(
+                            new vscode.Range(computedRange.start, computedRange.end),
+                            new vscode.SnippetString(`computed: {\n${snippetStirng}}`),
+                        ),
+                    ])
+                else
+                    workspaceEdit.set(document.uri, [
+                        new vscode.SnippetTextEdit(
+                            new vscode.Range(computedRange.start.translate(1), computedRange.start.translate(1)),
+                            new vscode.SnippetString(snippetStirng),
+                        ),
+                    ])
 
                 codeActions.push({
                     title: 'Register pinia store in computed',
